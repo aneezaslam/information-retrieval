728e782fdf1aff63b00b8c45371cf22187263458
aneezaslam <110680854+aneezaslam@users.noreply.github.com>
2022-08-26 15:35:19 +0100

    Add files via upload

---
+++ GeneticAlgorithm.m
@@ -0,0 +1,21 @@
+clc, clear, close all
+
+opt = optimoptions('ga','PlotFcn', {@gaplotbestf, @gaplotdistance});
+
+for i = 1:15
+
+    initial_flag = 0;
+
+    [x,fval,exit_flag,opt] = ga(@(x) benchmark_func(x,10),2,opt);
+
+    %[x,fval,exit_flag,opt] = ga(@(x) benchmark_func(x,10),10,opt);
+
+    %[x,fval,exit_flag,opt] = ga(@(x) benchmark_func(x,10),2,opt);
+
+    %[x,fval,exit_flag,opt] = ga(@(x) benchmark_func(x,10),10,opt);
+
+    val(i) = fval;
+
+    option(i) = opt;
+
+end
